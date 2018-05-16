e9e151a45883d3eeae1f6ac9f8a86e25f58b6c41
damian <damian.ciocca@etermax.com>
2018-05-16 09:53:10 -0300

    add example 7 => handle errors and continue with defult values

--- src/main/kotlin/example6errors/App.kt
+++ src/main/kotlin/example6errors/App.kt
@@ -71,5 +71,71 @@ fun main(args: Array<String>) {
      * completed!
      */
 
+    println("----> RX doOnError is called before exception was thrown.. Ideal to perform any action")
+
+    listOf("Aleberto", "Damian", "Rodri", "Ger").toFlowable()
+            .doOnNext {
+                if (it == "Rodri") {
+                    throw IllegalArgumentException("invalid")
+                }
+            }
+            .doOnError { println("OnError was called before exception thrown") }
+            .subscribeBy(
+                    onNext = { println(it) },
+                    onError = { println("error!!.. Stop") },
+                    onComplete = { println("completed!") })
+
+    /**
+     * Result:
+     * Aleberto
+     * Damian
+     * OnError was called before exception thrown
+     * error!!.. Stop
+     */
+
+    println("----> RX onErrorReturn is called before exception was thrown an allow complete it with ONE default value")
+
+    listOf("Aleberto", "Damian", "Rodri", "Ger").toFlowable()
+            .doOnNext {
+                if (it == "Rodri") {
+                    throw IllegalArgumentException("invalid")
+                }
+            }
+            .onErrorReturn { "default value" }
+            .subscribeBy(
+                    onNext = { println(it) },
+                    onError = { println("error!!.. Stop") },
+                    onComplete = { println("completed!") })
+
+    /**
+     * Result:
+     * Aleberto
+     * Damian
+     * default value
+     * completed!
+     */
+
+    println("----> RX onErrorResumeNext is called before exception was thrown an allow complete it with a LIST of default values")
+
+    listOf("Aleberto", "Damian", "Rodri", "Ger").toFlowable()
+            .doOnNext {
+                if (it == "Rodri") {
+                    throw IllegalArgumentException("invalid")
+                }
+            }
+            .onErrorResumeNext(listOf("valu1, value2, value3").toFlowable())
+            .subscribeBy(
+                    onNext = { println(it) },
+                    onError = { println("error!!.. Stop") },
+                    onComplete = { println("completed!") })
+
+    /**
+     * Result:
+     *
+     * Aleberto
+     * Damian
+     * valu1, value2, value3
+     * completed!
+     */
 }
 
